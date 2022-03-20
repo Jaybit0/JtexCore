13ddb574e72da12b4b6b874d94f2d52c96041084
Jaybit0 <jannik.lindemann@freenet.de>
2022-03-20 19:52:17 +0100

    fix: Pow-operator left operand in brackets for consistency

--- operators/binary/op_bin_default.js
+++ operators/binary/op_bin_default.js
@@ -2,6 +2,11 @@ const { ParserTokens, Tokens } = require("../../constants");
 const { Operator, OperatorType } = require("../operator");
 const {ParserToken} = require("../../parser_token");
 
+/**
+ * Generates all binary operators implemented in this file.
+ * This function is required for the operator_loader to recognize the module and should not be called manually.
+ * @returns the list of operators
+ */
 function generate() {
     var opFrac = new Operator(OperatorType.BINARY_OPERATOR, ParserTokens.FRACTION, binaryOperatorFrac)
         .injectToCommand("default.math.inline")
@@ -21,18 +26,36 @@ function generate() {
     return [opFrac, opMul, opPow, opIntegral, opSubscript];
 }
 
+/**
+ * Converts '<op1>/<op2>' to '\\frac{<op1>}{<op2>}'.
+ * @param {Token} op1 the left operand
+ * @param {Token} op2 the right operand
+ * @returns a parser token
+ */
 function binaryOperatorFrac(op1, op2) {
     return new ParserToken(ParserTokens.STRING).at(op1, op2).withData("\\frac{" + op1.unwrap().toString() + "}{" + op2.unwrap().toString() + "}");
 }
 
+/**
+ * Converts '<op1>*<op2>' to '<op1>\\cdot{}<op2>'.
+ * @param {Token} op1 the left operand
+ * @param {Token} op2 the right operand
+ * @returns a parser token
+ */
 function binaryOperatorMul(op1, op2) {
     return new ParserToken(ParserTokens.STRING).at(op1, op2).withData(op1.toString() + "\\cdot{}" + op2.toString());
 }
 
+/**
+ * Converts '<op1>^<op2>' to '{<op1>}^{<op2>}'.
+ * @param {Token} op1 the left operand
+ * @param {Token} op2 the right operand
+ * @returns a parser token
+ */
 function binaryOperatorPow(op1, op2) {
     if (op2.beginToken.id == Tokens.UNDERSCORE && op2.beginToken == op2.endToken)
         return new ParserToken(ParserTokens.STRING).at(op1, op2).withData("\\overline{" + op1.unwrap().toString() + "}");
-    return new ParserToken(ParserTokens.STRING).at(op1, op2).withData(op1.unwrap().toString() + "^{" + op2.unwrap().toString() + "}");
+    return new ParserToken(ParserTokens.STRING).at(op1, op2).withData("{" + op1.unwrap().toString() + "}^{" + op2.unwrap().toString() + "}");
 }
 
 function binaryOperatorIntegral(op1, op2) {
